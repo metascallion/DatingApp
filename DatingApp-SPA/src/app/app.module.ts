b1ca26f234b5fbbd14b8435d90f25ecd0a021533
Sascha <s.schwarzlose@softfair.de>
2019-11-05 15:10:35 +0100

    End of section 14

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import {BsDropdownModule, ButtonsModule, PaginationModule, TabsModule} from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule } from 'ngx-gallery';
@@ -58,8 +58,10 @@ export function tokenGetter() {
       ReactiveFormsModule,
       BsDropdownModule.forRoot(),
       RouterModule.forRoot(appRoutes),
+     PaginationModule.forRoot(),
       NgxGalleryModule,
       FileUploadModule,
+     ButtonsModule.forRoot(),
       TabsModule.forRoot(),
       JwtModule.forRoot({
          config: {
--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -6,11 +6,17 @@ namespace DatingApp.API.Helpers
         private int _pageSize = 10;
         
         public int PageNumber { get; set; } = 1;
+        public int UserId { get; set; }
+        public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 99;
 
         public int PageSize
         {
             get => _pageSize;
             set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
+
+        public string OrderBy { get; set; }
     }
 }
